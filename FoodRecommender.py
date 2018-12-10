4d9bbc6436259228d380c039d62db9cceac2ef5c
kzh <1432245553@qq.com>
2018-12-10 11:44:57 +0800

    修正bug

--- FoodRecommender.py
+++ FoodRecommender.py
@@ -18,8 +18,9 @@ class FoodRecommender:
         self.salty_lb = 0
         self.salty_ub = MAX_SCORE
         self.sweet, self.sour, self.hot, self.oily, self.salty = DBHelper.conn.execute(
-            "SELECT ifnull(avg(score*hot),2.5),ifnull(avg(score*salty),2.5),ifnull(avg(score*sweet),2.5),"
-            "ifnull(avg(score*sour),2.5),ifnull(avg(score*oily),2.5) FROM comments WHERE user_id=?;", (self.user_id,)).fetchone()
+            "SELECT ifnull(sum(score*sweet)/sum(score),2.5),ifnull(sum(score*sour)/sum(score),2.5),"
+            "ifnull(sum(score*hot)/sum(score),2.5),ifnull(sum(score*oily)/sum(score),2.5),"
+            "ifnull(sum(score*salty)/sum(score),2.5) FROM comments WHERE user_id=?;", (self.user_id,)).fetchone()
         self.except_list = []
 
     def get_recommend(self, num):
@@ -40,74 +41,74 @@ class FoodRecommender:
         return res
 
     def too_sweet(self, food_id):
-        s = DBHelper.conn.execute("SELECT ifnull(avg(sweet),-1) WHERE food_id=?", (food_id, )).fetchone()[0]
+        s = DBHelper.conn.execute("SELECT ifnull(avg(sweet),-1) FROM comments WHERE food_id=?", (food_id, )).fetchone()[0]
         if s == -1:
             self.except_list.append(str(food_id))
         else:
-            self.sweet_ub = s
+            self.sweet_ub = s-0.1
             self.sweet = (self.sweet_lb+ self.sweet_ub)/2
 
     def not_sweet(self, food_id):
-        s = DBHelper.conn.execute("SELECT ifnull(avg(sweet),-1) WHERE food_id=?", (food_id, )).fetchone()[0]
+        s = DBHelper.conn.execute("SELECT ifnull(avg(sweet),-1) FROM comments WHERE food_id=?", (food_id, )).fetchone()[0]
         if s == -1:
             self.except_list.append(str(food_id))
         else:
-            self.sweet_lb = s
+            self.sweet_lb = s+0.1
             self.sweet = (self.sweet_lb+ self.sweet_ub)/2
 
     def too_sour(self, food_id):
-        s = DBHelper.conn.execute("SELECT ifnull(avg(sour),-1) WHERE food_id=?", (food_id, )).fetchone()[0]
+        s = DBHelper.conn.execute("SELECT ifnull(avg(sour),-1) FROM comments WHERE food_id=?", (food_id, )).fetchone()[0]
         if s == -1:
             self.except_list.append(str(food_id))
         else:
-            self.sour_ub = s
+            self.sour_ub = s-0.1
             self.sour = (self.sour_lb+ self.sour_ub)/2
 
     def not_sour(self, food_id):
-        s = DBHelper.conn.execute("SELECT ifnull(avg(sour),-1) WHERE food_id=?", (food_id, )).fetchone()[0]
+        s = DBHelper.conn.execute("SELECT ifnull(avg(sour),-1) FROM comments WHERE food_id=?", (food_id, )).fetchone()[0]
         if s == -1:
             self.except_list.append(str(food_id))
         else:
-            self.sour_lb = s
+            self.sour_lb = s+0.1
             self.sour = (self.sour_lb+ self.sour_ub)/2
 
     def tasteless(self, food_id):
         s = DBHelper.conn.execute("SELECT ifnull(avg(hot),-1), ifnull(avg(oily),-1), ifnull(avg(salty),-1) "
-                                  "WHERE food_id=?", (food_id, )).fetchone()
+                                  "FROM comments WHERE food_id=?", (food_id, )).fetchone()
         if min(s) == -1:
             self.except_list.append(str(food_id))
         if s[0] != -1:
-            self.hot_lb = s[0]
+            self.hot_lb = s[0]+0.1
             self.hot = (self.hot_lb+ self.hot_ub)/2
         if s[1] != -1:
-            self.oily_lb = s[1]
+            self.oily_lb = s[1]+0.1
             self.oily = (self.oily_lb+ self.oily_ub)/2
         if s[2] != -1:
-            self.salty_lb = s[2]
+            self.salty_lb = s[2]+0.1
             self.salty = (self.salty_lb+ self.salty_ub)/2
 
     def too_hot(self, food_id):
-        s = DBHelper.conn.execute("SELECT ifnull(avg(hot),-1) WHERE food_id=?", (food_id, )).fetchone()[0]
+        s = DBHelper.conn.execute("SELECT ifnull(avg(hot),-1) FROM comments WHERE food_id=?", (food_id, )).fetchone()[0]
         if s  ==  -1:
             self.except_list.append(str(food_id))
         else:
-            self.hot_ub = s
+            self.hot_ub = s-0.1
             self.hot = (self.hot_lb+ self.hot_ub)/2
 
     def too_oily(self, food_id):
-        s = DBHelper.conn.execute("SELECT ifnull(avg(oily),-1) WHERE food_id=?", (food_id, )).fetchone()[0]
+        s = DBHelper.conn.execute("SELECT ifnull(avg(oily),-1) FROM comments WHERE food_id=?", (food_id, )).fetchone()[0]
         if s == -1:
             self.except_list.append(str(food_id))
         else:
-            self.oily_ub = s
+            self.oily_ub = s-0.1
             self.oily = (self.oily_lb+ self.oily_ub)/2
 
     def too_salty(self, food_id):
-        s = DBHelper.conn.execute("SELECT ifnull(avg(salty),-1) WHERE food_id=?", (food_id, )).fetchone()[0]
+        s = DBHelper.conn.execute("SELECT ifnull(avg(salty),-1) FROM comments WHERE food_id=?", (food_id, )).fetchone()[0]
         if s == -1:
             self.except_list.append(str(food_id))
         else:
-            self.salty_ub = s
+            self.salty_ub = s-0.1
             self.salty = (self.salty_lb+ self.salty_ub)/2
 
     def no_reason(self, food_id):
